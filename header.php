a7e6e1c3386cf849ec1916fa187a6e84a12e300b
xiaoFen <hellowd93@163.com>
2016-09-05 17:36:58 +0800

    add <head />

--- header.php
+++ header.php
@@ -10,11 +10,62 @@
 <!--<![endif]-->
 
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <meta name="viewport" content="width=device-width" />
+    <!-- 设置标题 -->
     <title>
-        <?php wp_title( '|', true, 'right' ); ?>
+      <?php
+        global $page, $paged;
+        $site_description = get_bloginfo( 'description', 'display' );
+        if ($site_description && ( is_home() || is_front_page() )) {
+          bloginfo('name');
+          echo " - $site_description";
+        } else {
+          echo trim(wp_title('',0));
+          if ( $paged >= 2 || $page >= 2 )
+            echo ' - ' . sprintf( __( '第%s页' ), max( $paged, $page ) );
+          echo ' | ' ;
+          bloginfo('name');
+        }
+      ?>
     </title>
+    <!-- 设置标题结束 -->
+
+    <!-- 设置关键词、描述 -->
+    <?php if (is_home() || is_front_page())
+      {
+        $description = get_option( 'zan_description' );
+        $keywords = get_option( 'zan_keywords' );
+      }
+      elseif (is_category())
+      {
+        $description = strip_tags(trim(category_description()));
+        $keywords = single_cat_title('', false);
+      }
+      elseif (is_tag())
+      {
+        $description = sprintf( __( '与标签 %s 相关联的文章列表'), single_tag_title('', false));
+        $keywords = single_tag_title('', false);
+      }
+      elseif (is_single())
+      {
+        if ($post->post_excerpt) {$description = $post->post_excerpt;}
+        else {$description = mb_strimwidth(strip_tags($post->post_content),0,110,"");}
+        $keywords = "";
+        $tags = wp_get_post_tags($post->ID);
+        foreach ($tags as $tag ) {$keywords = $keywords . $tag->name . ", ";}
+      }
+      else
+      {
+        $description = get_option( 'zan_description' );
+        $keywords = get_option( 'zan_keywords' );
+      }
+    ?>
+    <!-- 设置关键词、描述结束 -->
+
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta content="<?php echo trim($description); ?>" name="description"/>
+    <meta content="<?php echo rtrim($keywords,','); ?>" name="keywords"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php get_header() ?>
 
+        <div class="container">
+
+<?php while ( have_posts() ) : the_post(); ?>
+        <section class="<?php the_ID(); ?>">
+            <h3 class="title">
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </h3>
+            <div class="content">
+                <?php the_content(); ?>
+            </div>
+        </section>
+<?php endwhile; ?>
+
+        </div>
+
 <?php get_footer(); ?>
